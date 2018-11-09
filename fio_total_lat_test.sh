b58d22de6b563956a6fe8d02237f74520afed11e
Leo <leoshch@gmail.com>
2018-11-09 15:41:38 +0800

    Create fio_total_lat_test.sh

---
+++ fio_total_lat_test.sh
@@ -0,0 +1,78 @@
+./fio_single_test.sh A_single_lun 1  read           0      4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0      4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0      4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0      4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70     4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50     4K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30     4K   1     300  #9
+
+
+
+./fio_single_test.sh A_single_lun 1  read           0     8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    8K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    8K   1     300  #9
+
+
+./fio_single_test.sh A_single_lun 1  read           0     16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    16K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    16K   1     300  #9
+
+
+./fio_single_test.sh A_single_lun 1  read           0     32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    32K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    32K   1     300  #9
+
+./fio_single_test.sh A_single_lun 1  read           0     64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    64K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    64K   1     300  #9
+
+
+./fio_single_test.sh A_single_lun 1  read           0     128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    128K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    128K   1     300  #9
+
+./fio_single_test.sh A_single_lun 1  read           0     256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    256K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    256K   1     300  #9
+
+
+./fio_single_test.sh A_single_lun 1  read           0     512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    512K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    512K   1     300  #9
+
+
+./fio_single_test.sh A_single_lun 1  read           0     1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randread       0     1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  write          0     1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randwrite      0     1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         70    1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         50    1024K   1     300  #9
+./fio_single_test.sh A_single_lun 1  randrw         30    1024K   1     300  #9
