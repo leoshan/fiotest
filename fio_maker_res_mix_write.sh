686f546365453c2efcbcda9e0e7c60d667ef7429
Leo <leoshch@gmail.com>
2018-11-12 18:55:02 +0800

    Create fio_maker_res_mix_write.sh

---
+++ fio_maker_res_mix_write.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#rm -rf fio_res*
+
+#mount /dev/sdb1/ /home/vm_env/vm_share_ext3/
+#cp /home/vm_env/vm_share_ext3/fio_res_* .
+#umount /dev/sdb1/
+
+#sh /home/dragon_test/fioA/pm_collect_result.sh
+#sh /home/dragon_test/fioB/pm_collect_result.sh 
+
+iops=0
+bw=0
+bwdata=0
+bwunit=0
+delaydata=0
+delayunit=0
+
+rm -rf fio_res_output_mix_write.txt
+for i in `ls fio_res*_randrw_*`
+do
+	#printf "%-50s" $i >> fio_res_output.txt
+	#printf "%-20s" `grep iops $i | awk -F',' '{print $3}'` >> fio_res_output.txt
+	#printf "%-20s" `grep iops $i | awk -F',' '{print $2}'` >> fio_res_output.txt
+	#printf "%20s" "`grep -w lat $i | grep -w avg | awk -F',' '{print $3}'`" >> fio_res_output.txt
+	#printf "%s" "`grep -w lat $i | head -n 1 | awk -F'(' '{print $2}' |  awk -F')' '{print $1}'`" >> fio_res_output.txt
+	#read
+
+
+
+	iops=`tail -n 26 $i|grep iops | awk -F',' '{print $3}' |awk -F'=' '{print $2}'`
+        bw=`tail -n 26 $i|grep iops  | awk -F',' '{print $2}' |awk -F'=' '{print $2}'`
+        bwdata=`tail -n 26 $i |grep iops | awk -F',' '{print $2}' |awk -F'=' '{print $2}'|awk -F"[KM]" '{print $1}'`
+        bwunit=${bw:0-4:4}
+	
+
+        delaydata=`tail -n 26 $i |grep -w lat  | grep -w avg | awk -F',' '{print $3}'|awk -F'=' '{print $2}'`
+        delayunit=`tail -n 26 $i |grep -w lat  | tail -n 1 | awk -F'(' '{print $2}' |  awk -F')' '{print $1}'`
+
+        clat_50=`tail -n 26 $i |grep 50.00th  | awk -F',' '{print $3}' |awk -F'[' '{print $2}'|awk -F']' '{print $1}'`
+        clat_90=`tail -n 26 $i |grep 90.00th | awk -F',' '{print $3}' |awk -F'[' '{print $2}'|awk -F']' '{print $1}'`
+        clat_99=`tail -n 26 $i |grep 99.00th | awk -F',' '{print $1}' |awk -F'[' '{print $2}'|awk -F']' '{print $1}'`
+        clat_9995=`tail -n 26 $i |grep 99.95th  | awk -F',' '{print $4}' |awk -F'[' '{print $2}'|awk -F']' '{print $1}'`
+
+	if [ $bwunit = "MB/s" ];then
+		bwdata=`echo "$bwdata*1000"|bc`;
+	fi
+
+	if [ $delayunit = "msec" ];then	
+		delaydata=`echo "$delaydata*1000"|bc`;
+	fi
+	
+	printf "%-15s %-15s %-15s %-8s %-8s %-8s %-s" $iops $bwdata $delaydata $clat_50 $clat_90 $clat_99 $clat_9995 >> fio_res_output_mix_write.txt	
+	printf "%-50s" $i >> fio_res_output_mix_write.txt
+
+	echo  >> fio_res_output_mix_write.txt
+done
+
+cat fio_res_output_mix_write.txt
