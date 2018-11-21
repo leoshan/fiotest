16cdde7e4348ad0de9d52c94aa705ceb60e25fce
Leo <leoshch@gmail.com>
2018-11-21 09:51:36 +0800

    Create fio_4k_iops_test.sh

---
+++ fio_4k_iops_test.sh
@@ -0,0 +1,12 @@
+./fio_single_test.sh	A_single_lun	1	write	0	4K	32	180
+./fio_single_test.sh	A_single_lun	2	read	0	4K	32	180
+./fio_single_test.sh	A_single_lun	3	randwrite	0	4K	32	180
+./fio_single_test.sh	A_single_lun	4	randread	0	4K	32	180
+./fio_single_test.sh	A_single_lun	5	write	0	4K	64	180
+./fio_single_test.sh	A_single_lun	6	read	0	4K	64	180
+./fio_single_test.sh	A_single_lun	7	randwrite	0	4K	64	180
+./fio_single_test.sh	A_single_lun	8	randread	0	4K	64	180
+./fio_single_test.sh	A_single_lun	9	write	0	4K	128	180
+./fio_single_test.sh	A_single_lun	10	read	0	4K	128	180
+./fio_single_test.sh	A_single_lun	11	randwrite	0	4K	128	180
+./fio_single_test.sh	A_single_lun	12	randread	0	4K	128	180
